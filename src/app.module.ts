c3a3d66e06fbb651e90257da895690dd430d3641
Michael Stirchak <michaelstirchak@Michaels-MacBook-Pro-2.local>
2020-08-16 14:25:05 -0700

    badic auth working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {Connection} from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { 
+  constructor(private connection: Connection) {}
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError'
+
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+  }
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.usersRepository.findOne(id);
+  }
+
+  async findUser(userName: String): Promise<User> {
+    try {
+      const user: User = await this.usersRepository.findOneOrFail({
+        where: {
+          username: userName
+        }
+      });
+      return user;
+
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        throw new NotFoundException('username ' + userName + ' not found.');
+      }
+      else {
+        throw new Error(error);
+      }
+    }
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+}
